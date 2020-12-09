45aa283bd70f53ae577c6518210384efa6bca01c
VitaliyShabanov <>
2020-12-09 08:02:51 +0300

    Base logic

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float dieDelay = 0.1f;
+    public int currentNPos = 0;
+
+    private EnemySettings _currentSettings;
+
+    public void StartMove(Vector3 startPos, EnemySettings enemySettings)
+    {
+        transform.localPosition = startPos;
+        InitSettings(enemySettings);
+        MoveToNextPoint();
+    }
+
+    public void InitSettings(EnemySettings enemySettings)
+    {
+        _currentSettings = enemySettings;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void MoveToNextPoint()
+    {
+        if (EnemyMaster.Instance.IsInFinalPoint(this))
+        {
+            Invoke(nameof(Die), dieDelay);
+        }
+
+        Vector3 nextPos = EnemyMaster.Instance.GetPositionOnPath(currentNPos);
+
+        if (nextPos != Vector3.zero)
+            StartCoroutine(StepMove(transform.localPosition, nextPos));
+
+        currentNPos++;
+    }
+
+    private IEnumerator StepMove(Vector3 from, Vector3 to)
+    {
+        float path = (to - from).magnitude;
+        float t = 0;
+        float step = 0;
+        while (t <= 1.0f)
+        {
+
+            {
+                step += _currentSettings.speed * Time.deltaTime;
+                t = (step * path) / path;
+                transform.localPosition = Vector3.Lerp(from, to, t);
+                yield return null;
+            }
+        }
+
+        transform.localPosition = to;
+        MoveToNextPoint();
+    }
+}
---
+++ Assets/Scripts/EnemyMaster.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemySettings
+{
+    public string name;
+    public float speed;
+    public int attack;
+    public int health;
+    public int goldAward;
+}
+
+public class EnemyMaster : MonoBehaviour
+{
+
+    public static EnemyMaster Instance;
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+
+    [SerializeField]
+    private Transform spawnPos = null;
+
+    [SerializeField] 
+    private Transform endPos = null;
+
+    [SerializeField]
+    private GameObject pathContainer;
+    
+    public List<Vector3> _pathPositions = new List<Vector3>();
+
+    public int enemiesLevel;
+    [SerializeField]
+    float spawnDelay = 4f;
+
+    [SerializeField]
+    private int maxEnemies = 10;
+    [SerializeField]
+    private int maxEnemiesRandomRange = 4;
+
+    [SerializeField]
+    private int currentLevelEnemiesAmount;
+
+    [SerializeField]
+    private List<EnemySettings> enemySettings;
+
+    [SerializeField]
+    private GameObject enemyPrefab;
+    
+    private List<EnemyController> _enemyList = new List<EnemyController>();
+
+    [SerializeField]
+    private EnemySettings baseEnemySettings;
+
+    private void Awake()
+    {
+        Transform[] pathObjs = pathContainer.GetComponentsInChildren<Transform>();
+        foreach(var pathObj in pathObjs)
+        {
+            if (pathObj.localPosition != Vector3.zero)
+                _pathPositions.Add(pathObj.localPosition);
+        }
+
+        Instance = this;
+
+        StartSpawnEnemies();
+    }
+
+    public Vector3 GetPositionOnPath(int n)
+    {
+        if (n < _pathPositions.Count)
+            return _pathPositions[n];
+        else
+            return Vector3.zero;
+    }
+
+    public bool IsInFinalPoint(EnemyController checkEnemy)
+    {
+        if (endPos == null)
+            return false;
+        else
+        {
+            if (checkEnemy.transform.localPosition == endPos.localPosition)
+            {
+                RemoveEnemy(checkEnemy);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void RemoveEnemy(EnemyController enemy)
+    {
+        _enemyList.Remove(enemy);
+    }
+
+    public void AddEnemy(EnemyController enemy)
+    {
+        _enemyList.Add(enemy);
+    }
+
+    public void StartSpawnEnemies()
+    {
+        currentLevelEnemiesAmount = maxEnemies + Random.Range(0, maxEnemiesRandomRange);
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (currentLevelEnemiesAmount > 0)
+        {
+            currentLevelEnemiesAmount--;
+            GameObject enemyGo = Instantiate(enemyPrefab, transform);
+            enemyGo.name = baseEnemySettings.name + currentLevelEnemiesAmount;
+
+            var enemyCont = enemyGo.GetComponent<EnemyController>();
+
+            if (enemyCont != null)
+            {
+                AddEnemy(enemyCont);
+                enemyCont.StartMove(spawnPos.localPosition, baseEnemySettings);
+            }
+
+            yield return new WaitForSeconds(spawnDelay);
+        }
+
+        StopAllCoroutines();
+    }
+}
